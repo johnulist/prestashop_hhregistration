a1c2c11be3b8ecc999528ba773f7c370fa493ec0
Hervé Hennes <Herv Hennes>
2016-02-18 12:55:34 +0100

    Version initiale du module

---
+++ hhregistration/hhregistration.php
@@ -0,0 +1,92 @@
+
+<?php
+
+/**
+ * Module Hh Registration : Ajout de champs au formulaire d'inscription
+ *
+ */
+class hhregistration extends Module {
+
+
+    /** Extensions autorisées pour l'envoi de fichiers */
+    private $_registration_allowed_extensions = array('pdf','doc','docx','jpg','png','gif','txt');
+
+    /* Nom du dossier dans lequel sont envoyés les fichiers */
+    private $_upload_dir = 'files/';
+
+    public function __construct() {
+        $this->name = 'hhregistration';
+        $this->tab = 'hhennes';
+        $this->author = 'hhennes';
+        $this->version = '0.1.0';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('HH Registration');
+        $this->description = $this->l('Sample module which show how to add fields to registration');
+    }
+
+    /**
+     * Installation du module : Installation Standard + greffe sur les hooks nécessaires
+     * @return boolean
+     */
+    public function install() {
+        if (
+                !parent::install() || !$this->registerHook('displayCustomerAccountForm') || !$this->registerHook('actionCustomerAccountAdd')
+        )
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Désintallation du module
+     * @return boolean
+     */
+    public function uninstall() {
+        if (!parent::uninstall())
+            return false;
+        return true;
+    }
+
+    /**
+     * Nouveaux champs à rajouter sur le formulaire de création de compte
+     * @param type $params
+     */
+    public function hookDisplayCustomerAccountForm($params) {
+
+        //Affichage du template du module ( situé dans views/templates/hook )
+        return $this->display(__FILE__, 'hookDisplayCustomerAccountForm.tpl');
+    }
+
+    /**
+     * Traitement des nouveaux champs du formulaire d'inscription
+     */
+    public function hookActionCustomerAccountAdd($params)
+    {
+        //Gestion de l'upload via la classe d'upload prestashop
+
+        //Pour un seul envoi
+        /*$uploader = new UploaderCore('file_input'); //Renseigner ici le nom du fichier envoyés
+        $uploader->setAcceptTypes($this->_registration_allowed_extensions)
+                ->setCheckFileSize(UploaderCore::DEFAULT_MAX_SIZE)
+                ->setSavePath(dirname(__FILE__) . '/' . $this->_upload_dir)
+                ->process();*/
+
+        //Pour plusieurs envois
+
+        $uploader = new UploaderCore(); //Renseigner ici le nom du fichier envoyés
+        $uploader->setAcceptTypes($this->_registration_allowed_extensions)
+                ->setCheckFileSize(UploaderCore::DEFAULT_MAX_SIZE)
+                ->setSavePath(dirname(__FILE__) . '/' . $this->_upload_dir);
+
+        //Mettre ici les différents fichiers à envoyer (Possible aussi de faire une boucle sur $_FILES
+        if ( isset($_FILES['file_input']))
+            $uploader->upload ($_FILES['file_input']);
+        if ( isset($_FILES['file_input2']))
+            $uploader->upload ($_FILES['file_input2']);
+
+    }
+
+}
---
+++ hhregistration/views/templates/hook/hookDisplayCustomerAccountForm.tpl
@@ -0,0 +1,11 @@
+{* Template d'affichage du module hhregistration *}
+<!-- Module hhregistration -->
+<div class="form-group">
+    <label for="file-input">{l s='Justificatif Professionnel' mod='hhregistration'}</label>
+    <input type="file" name="file_input" />
+</div>
+<!--<div class="form-group">
+    <label for="file-input">{l s='Justificatif Professionnel 2' mod='hhregistration'}</label>
+    <input type="file" name="file_input2" />
+</div>-->
+<!-- Module hhregistration -->
